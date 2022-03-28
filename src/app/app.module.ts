d5f0c66fd2ca1b5d2c5d096805ac443dac6cec3c
Natalinacn <natalinacuna@gmail.com>
2022-03-28 11:38:55 -0300

    Agregado de componentes experiencia y estudios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { HomeComponent } from './home/home.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { HomeComponent } from './home/home.component';
     SocialComponent,
     BannerComponent,
     AcercaDeComponent,
-    HomeComponent
+    HomeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
