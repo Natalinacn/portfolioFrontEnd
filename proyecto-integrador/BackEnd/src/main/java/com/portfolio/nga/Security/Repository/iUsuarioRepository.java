acfdb373890a3c5f733b3aba434046632d009c4f
Natalinacn <natalinacuna@gmail.com>
2022-08-05 11:46:51 -0300

    Back con errores

--- proyecto-integrador/BackEnd/src/main/java/com/portfolio/nga/Security/Repository/iUsuarioRepository.java
+++ proyecto-integrador/BackEnd/src/main/java/com/portfolio/nga/Security/Repository/iUsuarioRepository.java
@@ -14,10 +14,11 @@ import org.springframework.stereotype.Repository;
 public interface iUsuarioRepository extends JpaRepository<Usuario, Integer>{
     Optional<Usuario> findByNombreUsuario(String nombreUsuario);
     
-    boolean existByNombreUsuario(String nombreUsuario);
-    boolean existByEmail(String email);
+    boolean existsByNombreUsuario(String nombreUsuario);
+    boolean existsByEmail(String email);
 
     public boolean existsByNombreUsuario(String nombreUsuario);
 
     public boolean existsByEmail(String email);
 }
+
