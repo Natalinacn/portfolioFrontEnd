7ad9fee2a8620b6e51929357808484b1748fc8d0
Natalinacn <natalinacuna@gmail.com>
2022-08-06 18:36:34 -0300

    Experiencia.ts terminado

---
+++ proyecto-integrador/FrontEnd/src/app/model/experiencia.ts
@@ -0,0 +1,12 @@
+export class Experiencia {
+    id? : number;
+    nombreE : string;
+    descriptionE : string;
+
+    constructor(nombreE: string, descriptionE: string){
+        this.nombreE = nombreE;
+        this.descriptionE = descriptionE;
+
+
+    }
+}
